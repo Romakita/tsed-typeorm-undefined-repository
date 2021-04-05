34d3d230cd0da2ca2729b922cb18f7a13e15b8d3
Yuki Takei <yuki@weseek.co.jp>
2021-04-05 11:14:43 +0000

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import {Configuration, Inject, PlatformApplication} from "@tsed/common";
+import * as bodyParser from "body-parser";
+
+const rootDir = __dirname;
+
+@Configuration({
+  rootDir,
+  acceptMimes: ["application/json"]
+})
+export class Server {
+  @Inject()
+  app: PlatformApplication;
+
+  @Configuration()
+  settings: Configuration;
+
+  /**
+   * This method let you configure the express middleware required by your application to works.
+   * @returns {Server}
+   */
+  public $beforeRoutesInit(): void | Promise<any> {
+    this.app
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({
+        extended: true
+      }));
+  }
+}
